c7ab0adfe89b7cfe376b6a417f08b0c87fd33225
vasco <vasco.schiavo@protonmail.com>
2024-04-27 00:03:38 +0200

    add tests for __eq__

--- tests/test_cycle/test_magic_methods.py
+++ tests/test_cycle/test_magic_methods.py
@@ -2,6 +2,7 @@ import pytest
 
 from tests.test_cycle.test_cases import (
     TEST_BOOL,
+    TEST_EQ,
 )
 from tests.utils import error_message
 
@@ -15,3 +16,17 @@ def test_bool(cycle, expected_output) -> None:
     """Tests for the method `__bool__()`."""
     if bool(cycle) != expected_output:
         raise ValueError(error_message(expected=expected_output, got=bool(cycle)))
+
+
+@pytest.mark.parametrize(
+    argnames="lhs, rhs, expected_output",
+    argvalues=TEST_EQ,
+    ids=[f"{lhs}={rhs}" for lhs, rhs, _ in TEST_EQ]
+)
+def test_bool(lhs, rhs, expected_output) -> None:
+    """Tests for the method `__eq__()`."""
+    if (lhs == rhs) != expected_output:
+        raise ValueError(
+            f"The expression `{lhs}=={rhs}` should be {expected_output}, but got {lhs == rhs}."
+        )
+
