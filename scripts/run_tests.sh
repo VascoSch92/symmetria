958f5795d3fe7757ff856f4ff445a56323968ebc
vasco <vasco.schiavo@protonmail.com>
2024-05-07 23:23:58 +0200

    delete scripts

--- scripts/run_tests.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Check if the '--report' flag is provided
-if [[ "$@" == *"--report"* ]]; then
-    pytest --cov=symmetria --cov-report term-missing --cov-report html
-else
-    pytest --cov=symmetria --cov-report term-missing
-fi
