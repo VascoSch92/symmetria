b4b7a9b7815b5b84de01c167e5eb5726a8f61a28
vasco <vasco.schiavo@protonmail.com>
2024-12-26 23:29:46 +0100

    deprecated old generator method

--- symmetria/generators/algorithm/api.py
+++ symmetria/generators/algorithm/api.py
@@ -1,3 +1,4 @@
+import warnings
 from typing import List, Generator
 
 from symmetria import Permutation
@@ -8,7 +9,7 @@ from symmetria.generators.algorithm._algorithms import (
     _steinhaus_johnson_trotter,
 )
 
-__all__ = ["generate"]
+__all__ = ["generate", "permutation_generator"]
 
 _SUPPORTED_ALGORITHM: List[str] = [
     "lexicographic",
@@ -19,6 +20,8 @@ _SUPPORTED_ALGORITHM: List[str] = [
 
 def generate(degree: int, algorithm: str = "lexicographic") -> Generator[Permutation, None, None]:
     """Generate all the permutations of the given degree based on the chosen algorithm.
+
+    :warning: This function will be deprecated in a future version. Use 'permutation_generator' instead.
 
     The method generates all the permutations of the given degree using the specified algorithm.
 
@@ -38,6 +41,46 @@ def generate(degree: int, algorithm: str = "lexicographic") -> Generator[Permuta
         >>> import symmetria
         ...
         >>> permutations = symmetria.generate(degree=3, algorithm="lexicographic")
+        >>> for permutation in permutations:
+        ...     permutation
+        Permutation(1, 2, 3)
+        Permutation(1, 3, 2)
+        Permutation(2, 1, 3)
+        Permutation(2, 3, 1)
+        Permutation(3, 1, 2)
+        Permutation(3, 2, 1)
+    """
+    warnings.warn(
+        "This function will be deprecated in a future version. Use 'permutation_generator' instead.",
+        PendingDeprecationWarning,
+        stacklevel=1,
+    )
+    _check_algorithm_parameter(value=algorithm, supported=_SUPPORTED_ALGORITHM)
+    _check_degree_parameter(value=degree)
+    return _relevant_generator(algorithm=algorithm, degree=degree)
+
+
+def permutation_generator(degree: int, algorithm: str = "lexicographic") -> Generator[Permutation, None, None]:
+    """Generate all the permutations of the given degree based on the chosen algorithm.
+
+    The method generates all the permutations of the given degree using the specified algorithm.
+
+    :param degree: The degree of the permutations to be generated. Must be a non-zero positive integer.
+    :type degree: int
+    :param algorithm: The algorithm to use for generating permutations.
+        It must be one of: ``lexicographic``, ``heap``.
+        Default is ``lexicographic``.
+    :type algorithm: str, optional
+
+    :return: A generator yielding permutations.
+    :rtype: Generator["Permutation", None, None]
+
+    :raises ValueError: If the algorithm is not supported or the degree is invalid.
+
+    :examples:
+        >>> from symmetria import permutation_generator
+        ...
+        >>> permutations = permutation_generator(degree=3, algorithm="lexicographic")
         >>> for permutation in permutations:
         ...     permutation
         Permutation(1, 2, 3)
