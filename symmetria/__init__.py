27a5ee6efadc52bf8bb47f9139d0f81cdf70af3c
vasco <vasco.schiavo@protonmail.com>
2025-09-09 14:56:26 +0200

    bump version

--- symmetria/__init__.py
+++ symmetria/__init__.py
@@ -4,7 +4,7 @@ from symmetria.generators.random.api import random, random_generator, random_per
 from symmetria.generators.algorithm.api import generate, permutation_generator
 from symmetria.elements.cycle_decomposition import CycleDecomposition
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __all__ = [
     "__version__",
     "permutation_generator",
